27655c71b80db6be1f355641d1a23f52476b17f3
JulioM <juliomissart.mdq@gmail.com>
2023-04-29 12:59:56 -0300

    Implement Product
    Implement IRepository interface

---
+++ RestAPI.Domain/Product/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestAPI.Domain.Product
+{
+    internal class Product : IProduct
+    {
+        public string Name { get; }
+        public string Description { get; }
+        public double Price { get; }
+
+        public Product(ProductDto product)
+        {
+            Name = product.Name;
+            Description = product.Description;
+            Price = product.Price;
+        }
+    }
+}
---
+++ RestAPI.Domain/Repository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestAPI.Domain.Repository
+{
+    internal interface IRepository<TEntity, TEntityGuid>
+    {
+        IEnumerable<TEntity> GetAll();
+        TEntity GetById(TEntityGuid guid);
+        TEntity Add(TEntity entity);
+        TEntity Update(TEntity entity);
+        TEntity Delete(TEntityGuid guid);
+    }
+}
